20f65b44f48b8a213a8e7f9986f76e7abe581e79
DESKTOP-5J922CU\tyler <tyler.covetts@gmail.com>
2022-05-03 21:54:18 -0500

    Added InputSystem for player and integrated it with PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector2 rawInput;
+    [SerializeField] float centiSpeed = 0.2f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        handleMove();
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+        Debug.Log(rawInput);
+    }
+
+    void handleMove()
+    {
+        Vector2 delta = rawInput * centiSpeed * Time.deltaTime;
+
+        Vector2 newPos = new Vector2();
+        newPos.x = transform.position.x + delta.x;
+        newPos.y = transform.position.y + delta.y;
+        transform.position = newPos;
     }
 }
