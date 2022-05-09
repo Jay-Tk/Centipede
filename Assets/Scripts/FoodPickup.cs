1c871d8e246b7a425568793386f67ea3e95ef68c
DESKTOP-5J922CU\tyler <tyler.covetts@gmail.com>
2022-05-08 22:35:01 -0500

    added offset for multiple centipede body parts

--- Assets/Scripts/FoodPickup.cs
+++ Assets/Scripts/FoodPickup.cs
@@ -10,15 +10,24 @@ public class FoodPickup : MonoBehaviour
     [SerializeField] float bodySpace = .75f;
     [SerializeField] Tilemap floorMap;
 
+    int bodyCount = 1;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Food")
         {
-            Vector3 newBodyPos = centipedeHead.transform.position - new Vector3(bodySpace, 0);
-            Instantiate(prefabCentipedeBody, newBodyPos, Quaternion.identity, centipedeHead.transform);
+            GrowCentipede();
             Destroy(collision.gameObject);
             //floorMap.GetTilesBlock(floorMap.cellBounds);
             //ToDo Spawn new food
         }
+    }
+
+    void GrowCentipede()
+    {
+        float bodyOffset = bodySpace * bodyCount;
+        Vector3 newBodyPos = centipedeHead.transform.position - new Vector3(bodyOffset, 0);
+        Instantiate(prefabCentipedeBody, newBodyPos, Quaternion.identity, centipedeHead.transform);
+        bodyCount++;
     }
 }
