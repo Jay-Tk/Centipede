e0b938b81e919c9186e541bdc2abdd32930afb7d
DESKTOP-5J922CU\tyler <tyler.covetts@gmail.com>
2022-05-28 00:36:18 -0500

    added GameSession script

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] int score;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        int numGameSessions = FindObjectsOfType<GameSession>().Length;
+        if (numGameSessions > 1)
+        {
+            Destroy(gameObject);
+        } else {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        //scoreText.text = score.ToString();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
