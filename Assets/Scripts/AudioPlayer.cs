c0ff9122eecd03217d5a3092ef9324e4297b59c7
DESKTOP-5J922CU\tyler <tyler.covetts@gmail.com>
2022-06-04 17:17:31 -0500

    added AudioPlayer object and script

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    //[SerializeField] AudioClip shootingClip;
+    //[SerializeField] [Range(0f, 1f)] float shootingVolume = 1f;
+    //[SerializeField] AudioClip damageClip;
+    //[SerializeField][Range(0f, 1f)] float damageVolume = 1f;
+
+    static AudioPlayer instance;
+
+    void Awake()
+    {
+        ManageSingleton();
+    }
+
+    void ManageSingleton()
+    {
+        //int instanceCount = FindObjectsOfType(GetType()).Length;
+        //if (instanceCount > 1)
+        if (instance)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        } else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    //public void PlayShootingClip()
+    //{
+    //    PlayClip(shootingClip, shootingVolume);
+    //}
+
+    void PlayClip(AudioClip clip, float volume)
+    {
+        if (clip)
+        {
+            Vector3 cameraPos = Camera.main.transform.position;
+            AudioSource.PlayClipAtPoint(clip, cameraPos, volume);
+        }
+    }
+}
